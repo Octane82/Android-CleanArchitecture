3214e91c3de5d02c3da9bcea581244068beacc10
Dmitriy K <octanek@gmail.com>
2019-03-01 14:44:34 +0300

    Added test for check CI (TeamCity)

---
+++ app/src/test/java/com/everlapp/cleanarch/CheckTestCI.java
@@ -0,0 +1,17 @@
+package com.everlapp.cleanarch;
+
+import org.junit.Test;
+
+import org.junit.Assert.*;
+
+import static org.junit.Assert.assertTrue;
+
+public class CheckTestCI {
+
+    @Test
+    public void testBoolean() {
+        boolean trueVal = true;
+        assertTrue(trueVal);
+    }
+
+}
